c3bfef3e0e1c5b92cf4d2207dcdbf6ec8b15e299
josevallejo1984 <54599840+josevallejo1984@users.noreply.github.com>
2020-04-29 12:54:47 -0500

    Update autoconfig.sh

--- autoconfig.sh
+++ autoconfig.sh
@@ -24,7 +24,7 @@ sudo apt-get install valgrind
 #***managger plugin shell zsh OH_MY_ZSH*******
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #*********manager pluggin vim bundle**********
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vi
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #autocomplete options with oh_my_zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
