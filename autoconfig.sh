3f9283eea4e9fa283443a6b105b64775460a7592
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-04-29 18:10:32 -0500

    ejecutable script

--- autoconfig.sh
+++ autoconfig.sh
@@ -5,13 +5,10 @@
 
 #********Install Shell ZSH*******************
 sudo apt install zsh
-#********Install pyton ans pip for WSL*******************
+#********Install pyton an pip for WSL*******************
 sudo apt update && upgrade
-sudo apt install python3 python3-pip ipython3
-sudo apt-get install python3-pep8
-sudo apt-get install python3-pip
-pip3 install pep8
-
+sudo apt-get install pip3
+sudo pip3 install pep8==1.7.0
 #**********instal GIT*************************
 sudo apt install git
 
@@ -26,6 +23,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 #*********manager pluggin vim bundle**********
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
 #*****theme powerline10k for oh_my_zsh*********
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
