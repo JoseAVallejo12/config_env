a7191c73c7a51b3b34bfe7fe433de6df8096d3df
josevallejo1984 <54599840+josevallejo1984@users.noreply.github.com>
2020-04-29 16:35:31 -0500

    more conf

--- autoconfig.sh
+++ autoconfig.sh
@@ -23,10 +23,13 @@ sudo apt-get install valgrind
 
 #***managger plugin shell zsh OH_MY_ZSH*******
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 #*********manager pluggin vim bundle**********
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 #*****theme powerline10k for oh_my_zsh*********
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+###Set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc.########
 
 #autocomplete options with oh_my_zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
